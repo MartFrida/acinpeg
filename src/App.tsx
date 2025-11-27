547179f0e5dc7bcc4ea0786b6a12511fe820bc41
Mari <mart.frida@gmail.com>
2025-11-27 23:58:35 +0200

    styled preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Contacts from './pages/Contacts';
-import AboutPage from './pages/AboutPage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CourseDetails from './pages/CourseDetailsPage';
-import CoursosPage from './pages/CoursosPage';
-import NoticiasPage from './pages/NoticiasPage';
-import ScrollToTopButton from './components/ScrollToTopButton';
-import { useEffect, useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Contacts from "./pages/Contacts";
+import AboutPage from "./pages/AboutPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CourseDetails from "./pages/CourseDetailsPage";
+import CoursosPage from "./pages/CoursosPage";
+import NoticiasPage from "./pages/NoticiasPage";
+import ScrollToTopButton from "./components/ScrollToTopButton";
+import { useEffect, useState } from "react";
+import Logo from "./assets/logo.svg";
 
 function App() {
-const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Симуляция загрузки данных/ассетов
-    const timer = setTimeout(() => setLoading(false), 1200); 
+    const timer = setTimeout(() => setLoading(false), 1200);
     return () => clearTimeout(timer);
   }, []);
 
@@ -23,25 +24,25 @@ const [loading, setLoading] = useState(true);
     return (
       <div className="preloader">
         <div className="preloader-spinner"></div>
-        <div className="preloader-title">ACINPEG</div>
+        <img src={Logo} alt="Logo" className="preloader-logo" />
       </div>
     );
   }
   return (
-    <div className='flex flex-col justify-between min-h-screen'>
+    <div className="flex flex-col justify-between min-h-screen">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/coursos' element={<CoursosPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/coursos" element={<CoursosPage />} />
         <Route path="/coursos/:id" element={<CourseDetails />} />
-        <Route path='/noticias' element={<NoticiasPage/>} />
-        <Route path='/contacts' element={<Contacts />} />
+        <Route path="/noticias" element={<NoticiasPage />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
-       <ScrollToTopButton />
+      <ScrollToTopButton />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
